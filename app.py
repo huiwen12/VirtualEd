5bf9d1bde12eb29c4cfef7d9d3f504fb4b90cb7f
Huiwen Chen <huiwenc21@gmail.com>
2021-11-28 02:54:26 -0500

    Update app.py

--- app.py
+++ app.py
@@ -3,9 +3,9 @@ import time
 import re
 
 from cs50 import SQL
-from app import app 
 from flask import Flask, flash, redirect, render_template, request
 from flask_mail import Mail, Message
+from flask_session import Session
 from tempfile import mkdtemp
 from werkzeug.exceptions import default_exceptions, HTTPException, InternalServerError
 from werkzeug.security import check_password_hash, generate_password_hash
@@ -18,6 +18,12 @@ app = Flask(__name__)
 # Ensure templates are auto-reloaded
 app.config["TEMPLATES_AUTO_RELOAD"] = True
 
+
+# Configure session to use filesystem (instead of signed cookies)
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
+
 #Configure mail 
 app.config["MAIL_DEFAULT_SENDER"] = os.environ["MAIL_DEFAULT_SENDER"]
 app.config["MAIL_PASSWORD"] = os.environ["MAIL_PASSWORD"]
@@ -27,11 +33,19 @@ app.config["MAIL_USE_TLS"] = True
 app.config["MAIL_USERNAME"] = os.environ["MAIL_USERNAME"]
 mail = Mail(app)
 
-# Configure CS50 Library to use SQLite database
-db = SQL("sqlite:///finance.db")
+app.config['ENV'] = 'development'
+app.config['DEBUG'] = True
+app.config['TESTING'] = True
+
+# add db file
+
+# Make sure API key is set
+if not os.environ.get("API_KEY"):
+    raise RuntimeError("API_KEY not set")
+
 
 # List of subjects
-SUBJECTS = [
+subjects = [
     "Arts",
     "Humanities",
     "Language",
@@ -42,6 +56,14 @@ SUBJECTS = [
     "Other"  
 ]
  
+
+@app.after_request
+def after_request(response):
+    """Ensure responses aren't cached"""
+    response.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
+    response.headers["Expires"] = 0
+    response.headers["Pragma"] = "no-cache"
+    return response
 
 
 @app.route("/")
@@ -83,7 +105,7 @@ def upload():
         return render_template("success.html")
 
     else:
-        return redirect("upload.html", subjects=SUBJECTS )
+        return redirect("/")
     
 
 
@@ -97,3 +119,5 @@ def errorhandler(e):
 # Listen for errors
 for code in default_exceptions:
     app.errorhandler(code)(errorhandler)
+
+app.run()
