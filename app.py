daf438b79c2fc59c0fd0f33ea8c039d86eea3ebe
Huiwen Chen <huiwenc21@gmail.com>
2021-11-29 14:55:00 -0500

    updated app

--- app.py
+++ app.py
@@ -5,7 +5,6 @@ import re
 from cs50 import SQL
 from flask import Flask, flash, redirect, render_template, request
 from flask_mail import Mail, Message
-from flask_session import Session
 from tempfile import mkdtemp
 from werkzeug.exceptions import default_exceptions, HTTPException, InternalServerError
 from werkzeug.security import check_password_hash, generate_password_hash
@@ -19,19 +18,19 @@ app = Flask(__name__)
 app.config["TEMPLATES_AUTO_RELOAD"] = True
 
 
-# Configure session to use filesystem (instead of signed cookies)
-app.config["SESSION_PERMANENT"] = False
-app.config["SESSION_TYPE"] = "filesystem"
-Session(app)
+# # Configure session to use filesystem (instead of signed cookies)
+# app.config["SESSION_PERMANENT"] = False
+# app.config["SESSION_TYPE"] = "filesystem"
+# Session(app)
 
 #Configure mail 
-app.config["MAIL_DEFAULT_SENDER"] = os.environ["MAIL_DEFAULT_SENDER"]
-app.config["MAIL_PASSWORD"] = os.environ["MAIL_PASSWORD"]
-app.config["MAIL_PORT"] = 587
-app.config["MAIL_SERVER"] = "smtp.gmail.com"
-app.config["MAIL_USE_TLS"] = True
-app.config["MAIL_USERNAME"] = os.environ["MAIL_USERNAME"]
-mail = Mail(app)
+# app.config['MAIL_SERVER']='smtp.gmail.com'
+# app.config['MAIL_PORT'] = 465
+# app.config['MAIL_USERNAME'] = "hellovirtualed@gmail.com"
+# app.config['MAIL_PASSWORD'] = "victualed"
+# app.config['MAIL_USE_TLS'] = False
+# app.config['MAIL_USE_SSL'] = True
+# mail = Mail(app)
 
 app.config['ENV'] = 'development'
 app.config['DEBUG'] = True
@@ -40,8 +39,8 @@ app.config['TESTING'] = True
 # add db file
 
 # Make sure API key is set
-if not os.environ.get("API_KEY"):
-    raise RuntimeError("API_KEY not set")
+# if not os.environ.get("API_KEY"):
+#     raise RuntimeError("API_KEY not set")
 
 
 # List of subjects
@@ -69,7 +68,7 @@ def after_request(response):
 @app.route("/")
 def home():
     """Show home page"""
-    return render_template("index.html")
+    return render_template("home.html")
 
 
 # Upload 
@@ -85,50 +84,53 @@ def upload():
         syllabus = request.form.get("syllabus")
 
 
-        # Validate name
-        if not name:
-            return render_template("error.html", message="Missing name")
-        # Validate email
-        if not email:
-            return render_template("error.html", message="Missing email")
-        if not course_title or not subject or not description or not syllabus:
-            return render_template("error.html", message="Missing required course materials")
+        # # Validate name
+        # if not name:
+        #     return render_template("apology.html", message="Missing name")
+        # # Validate email
+        # if not email:
+        #     return render_template("apology.html", message="Missing email")
+        # if not course_title or not subject or not description or not syllabus:
+        #     return render_template("apology.html", message="Missing required course materials")
     
-        # Add submission to SQL
-        db.execute("INSERT INTO uploads (name, email, course_title, subject, description, syllabus) VALUES (?, ?, ?, ?, ?, ?)", name, email, course_title, subject, description, syllabus)
+        # # Add submission to SQL
+        # # db.execute("INSERT INTO uploads (name, email, course_title, subject, description, syllabus) VALUES (?, ?, ?, ?, ?, ?)", name, email, course_title, subject, description, syllabus)
 
-        # Send email to the user when upload is successful
-        message = Message("Your upload was successful. Thank you for your submission!", recipients=[email])
-        mail.send(message)
+        # # Send email to the user when upload is successful
+        # # message = Message("Your upload was successful. Thank you for your submission!", sender = "hellovirtualed@gmail.com", recipients=[email])
+        # # message.body = "This is the email body sending with flask!"
+        # # mail.send(message)
 
-        # Confirm submission
+        # # Confirm submission
         return render_template("success.html")
 
     else:
-        return redirect("/")
+        return render_template("upload.html")
 
 
-@app.route("/contact", methods=["POST"])
+@app.route("/contact", methods=["POST", "GET"])
 def contact():
-    name = request.form.get("name")
-    email = request.form.get("email")
-    email_message = request.form.get("email_message")
-    if not name or not email or not email_message:
-        return render_template("error.html")
+    if request.method == "POST":
+        name = request.form.get("name")
+        email = request.form.get("email")
+        email_message = request.form.get("email_message")
+        if not name or not email or not email_message:
+            return render_template("apology.html")
 
-    message = Message(email_message, recipients=["hchen@college.harvard.edu"])
-    mail.send(message)
-
-
-def errorhandler(e):
-    """Handle error"""
-    if not isinstance(e, HTTPException):
-        e = InternalServerError()
-    return apology(e.name, e.code)
+        # message = Message(email_message, sender = "hellovirtualed@gmail.com", recipients=["hchen@college.harvard.edu"])
+        # mail.send(message)
+    else:
+        return render_template("contact.html")
 
 
-# Listen for errors
-for code in default_exceptions:
-    app.errorhandler(code)(errorhandler)
+# def errorhandler(e):
+#     """Handle error"""
+#     if not isinstance(e, HTTPException):
+#         e = InternalServerError()
+#     return apology(e.name, e.code)
 
-app.run()
+
+# # Listen for errors
+# for code in default_exceptions:
+#     app.errorhandler(code)(errorhandler)
+
